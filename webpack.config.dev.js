0be6348c44d670d73c2fc8c1b53845b5e3c9726e
= <=>
2025-06-20 09:39:08 +0530

    Commit

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const path = require("node:path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 
 module.exports = merge(common, {
   mode: 'development',
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,5 @@
 const {merge} = require("webpack-merge");
 const common = require("./webpack.common.js");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
