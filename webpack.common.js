641706a6f5fb4b9663b83b0e12f62ea088059e39
= <=>
2025-06-20 09:53:39 +0530

    Commit

--- webpack.common.js
+++ webpack.common.js
@@ -2,31 +2,36 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: {
-    app: "./src/app.ts"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_module/
-      }
-    ]
-  },
+    entry: {
+        app: "./src/app.ts"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_module/
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+                exclude: /node_modules/
+            }
+        ]
+    },
 
-  resolve: {
-    extensions: [".ts", ".js"],
-    mainFields: ["browser", "module", "main"]
-  },
-  output: {
-    filename: "bundle.js",
-    clean: true,
-    path: path.resolve(__dirname, "docs")
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    })
-  ]
+    resolve: {
+        extensions: [".ts", ".js"],
+        mainFields: ["browser", "module", "main"]
+    },
+    output: {
+        filename: "bundle.js",
+        clean: true,
+        path: path.resolve(__dirname, "docs")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
 };
